4c71ffa4b993a02e4aad0288f899522346185f91
lribeirogomes <luis.ribeiro.gomes@tecnico.pt>
2016-02-29 03:53:23 +0000

    remove old folder

--- project/compileClient.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET="target"
-
-cd ${TARGET}
-
-java -Djava.security.policy==../resources/java.policy SecClient
-
-cd ..
--- project/src/BlockServer.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-import java.util.Vector;
-
-public interface BlockServer extends Remote {
-  	byte[] get(int id) throws RemoteException;
-    int put_k(byte[] data, byte[] signature, byte[] public_key) throws RemoteException;
-    int put_h(byte[] data) throws RemoteException;
-}
